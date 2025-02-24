90137912a9c3c6f839be190e9db3a7040f85ef8e
gagansdevang <gagansdevang.14@gmail.com>
2025-02-24 21:13:47 +0530

    protect error fix

--- routes/protectedRoute.js
+++ routes/protectedRoute.js
@@ -1,12 +1,12 @@
 // routes/protectedRoute.js
 
 const express = require('express');
-const authMiddleware = require('../middlewares/authMiddleware'); // Import authMiddleware
+const { protect } = require("../middlewares/authMiddleware");
 
 const router = express.Router();
 
 // Protected route example
-router.get('/protected-route', authMiddleware, (req, res) => {
+router.get('/protected-route', protect, (req, res) => {
   // This route is only accessible to authenticated users with a valid token
   res.json({ message: 'This is a protected route, accessible only with a valid token.' });
 });
