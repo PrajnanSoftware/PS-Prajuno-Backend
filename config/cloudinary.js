66e10a94561de49c12e85acee712bbb5958d2afa
gagansdevang <gagansdevang.14@gmail.com>
2025-02-26 00:55:50 +0530

    s3

--- config/cloudinary.js
+++ config/cloudinary.js
@@ -1,12 +1,15 @@
-import { v2 as cloudinary } from "cloudinary";
-import dotenv from "dotenv";
-
-dotenv.config();
+const cloudinary = require("cloudinary").v2;
 
 cloudinary.config({
-  cloud_name: process.env.CLOUD_NAME,
-  api_key: process.env.CLOUD_API_KEY,
-  api_secret: process.env.CLOUD_API_SECRET,
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
-export default cloudinary;
+if (!process.env.CLOUDINARY_CLOUD_NAME || 
+    !process.env.CLOUDINARY_API_KEY || 
+    !process.env.CLOUDINARY_API_SECRET) {
+  throw new Error("Cloudinary is not configured correctly");
+}
+
+module.exports = cloudinary;
