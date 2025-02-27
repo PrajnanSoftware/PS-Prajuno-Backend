401efb0f53e12400e35c327be4b5b2ba461b29ec
gagansdevang <gagansdevang.14@gmail.com>
2025-02-27 16:07:04 +0530

    removed profilr by id

--- routes/profileRoutes.js
+++ routes/profileRoutes.js
@@ -3,7 +3,7 @@ const { getProfile, editProfile } = require('../controllers/profileController');
 const { protect } = require("../middlewares/authMiddleware");
 const router = express.Router();
 
-router.get('/:id', protect, getProfile);
-router.put('/:id', protect, editProfile);
+router.get('/', protect, getProfile);
+router.put('/', protect, editProfile);
 
 module.exports = router;
