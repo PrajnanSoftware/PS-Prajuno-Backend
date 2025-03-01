3100d5ea06f127095850559c5a9fec23828ce217
gagansdevang <gagansdevang.14@gmail.com>
2025-03-01 16:12:35 +0530

    user model edited

--- models/User.js
+++ models/User.js
@@ -23,36 +23,37 @@ const userSchema = new mongoose.Schema({
   password: { type: String, required: true, default: 'Abc@123' }, // Default password
   role: { type: String, enum: ['HR', 'Manager', 'CEO', 'Employee', 'Intern'], default: 'Intern' },
   profilePic: { type: String },
+
   personal: {
-    dob: { type: Date },
-    gender: { type: String, enum: ['Male', 'Female', 'Other'] },
-    bloodGroup: { type: String },
-    maritalStatus: { type: String, enum: ['Single', 'Married', 'Divorced', 'Widowed'] },
-    phoneNumber: { type: String },
-    alternatePhoneNumber: { type: String },
-    personalEmail: { type: String },
-    officialEmail: { type: String },
+    dob: { type: Date, default: null },
+    gender: { type: String, enum: ['Male', 'Female', 'Other'], default: '' },
+    bloodGroup: { type: String, default: '' },
+    maritalStatus: { type: String, enum: ['Single', 'Married', 'Divorced', 'Widowed'], default: 'Single' },
+    phoneNumber: { type: String, default: '' },
+    alternatePhoneNumber: { type: String, default: '' },
+    personalEmail: { type: String, default: '' },
+    officialEmail: { type: String, default: '' },
     address: {
-      current: addressSchema,
-      permanent: addressSchema
+      current: { type: addressSchema, default: {} },
+      permanent: { type: addressSchema, default: {} }
     }
   },
 
   work: {
-    empId: { type: String, unique: true }, // Auto-generated if required
-    empType: { type: String, enum: ['Permanent', 'Contract', 'Intern'] },
-    doj: { type: Date },
+    empId: { type: String, unique: true, sparse: true }, // Auto-generated if required
+    empType: { type: String, enum: ['Permanent', 'Contract', 'Intern'], default: '' },
+    doj: { type: Date, default: null },
     status: { type: String, enum: ['Active', 'Inactive', 'On Leave', 'Resigned'], default: 'Active' },
-    workLocation: { type: String },
+    workLocation: { type: String, default: '' },
     workExperience: {
       years: { type: Number, default: 0 },
       months: { type: Number, default: 0 }
     },
     probationPeriod: { type: Number, default: 0 }, // In months
-    designation: { type: String },
-    jobTitle: { type: String },
-    department: { type: String },
-    subDepartment: { type: String }
+    designation: { type: String, default: ''},
+    jobTitle: { type: String, default: '' },
+    department: { type: String, default: '' },
+    subDepartment: { type: String, default: '' }
   },
 
   team: {
@@ -60,7 +61,7 @@ const userSchema = new mongoose.Schema({
     directReports: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }]
   },
 
-  workweek: workweekSchema,
+  workweek: { type: workweekSchema, default: {} },
 
   resetToken: String,
   resetTokenExpiry: Date
